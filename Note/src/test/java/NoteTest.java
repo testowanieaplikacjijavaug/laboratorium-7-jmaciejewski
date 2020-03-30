c28459aa9517542559a23840780f347e8e735608
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-30 16:06:20 +0200

    Written tests for task 1

--- Note/src/test/java/NoteTest.java
+++ Note/src/test/java/NoteTest.java
@@ -1,2 +1,44 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NoteTest {
+Note n;
+
+    @Test
+    public void test_pass() {
+        n = Note.of("Jakub", 5.0f);
+
+    }
+
+    @Test
+    public void test_name_null() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            n = Note.of(null, 5.0f);
+        });
+    }
+
+    @Test
+    public void test_name_empty() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            n = Note.of(" ", 5.0f);
+        });
+    }
+
+    @Test
+    public void test_note_too_low() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            n = Note.of("Jakub", 1.0f);
+        });
+    }
+
+    @Test
+    public void test_note_too_high() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            n = Note.of("Jakub", 7.0f);
+        });
+    }
 }
